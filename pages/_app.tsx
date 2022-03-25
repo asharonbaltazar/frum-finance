19502b72e8e08ff5ba9faaee9bf65150c4c9077d
asharonbaltazar <asharonbaltazar@outlook.com>
2022-03-24 23:06:14 -0400

    Add component TS types

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
+import React from 'react'
+import { AppProps } from 'next/app'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import React from 'react'
+
+const Home: React.FC = () => {
   return (
     <div className='text-xl'>
       Welcome to NextJS
     </div>
   )
 }
+
+export default Home
